708426dbbeb34044d4baa1f3946e2833d4afa208
Darren Chan Yu Hao <darrenchanyuhao@gmail.com>
2025-06-09 17:07:56 +0800

    Feat - update application title in main.py for clarity on project purpose

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from routes.scan import scan_controller
 from routes.genai import genai_controller
 
 app = FastAPI()
-app.title = "cloudsineAI GenAI Virus and Malware Scanner Backend"
+app.title = "cloudsineAI Take Home Assignment: GenAI Virus and Malware Scanner Backend"
 
 app.include_router(scan_controller.router, tags=["Scan"])
 app.include_router(genai_controller.router, tags=["GenAI"])
